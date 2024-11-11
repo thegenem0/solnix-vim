ed3a3694d836a5dc4629282fb4ea31847af946a7
thegenem0 <nemethgergo02@gmail.com>
2024-11-11 10:36:56 +0000

    fix: disable tabbing with CMP

--- config/cmp.nix
+++ config/cmp.nix
@@ -114,20 +114,6 @@
           "<C-f>" = "cmp.mapping.scroll_docs(4)";
           "<C-Space>" = "cmp.mapping.complete()";
           "<S-Tab>" = "cmp.mapping.close()";
-          "<Tab>" =
-            # lua
-            ''
-              function(fallback)
-                local line = vim.api.nvim_get_current_line()
-                if line:match("^%s*$") then
-                  fallback()
-                elseif cmp.visible() then
-                  cmp.confirm({ behavior = cmp.ConfirmBehavior.Insert, select = true })
-                else
-                  fallback()
-                end
-              end
-            '';
           "<Down>" =
             # lua
             ''
